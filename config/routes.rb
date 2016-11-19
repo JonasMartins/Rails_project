468520d5737666396a5efea6f470b198267ca69e
JonasMartins <jonas.s.martins@hotmail.com>
2016-11-18 21:10:36 -0300

    Start My Own Style pages and routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,12 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
 
-  root 'welcome#index'
+  root 'page#home'
 
   # controller pages action home
-  get '/home', to:'page#home'
+  get '/welcome', to:'welcome#index'
 
+  #
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
