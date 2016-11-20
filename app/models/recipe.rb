7028d700be18c37275bba885b72015ff77d15ceb
JonasMartins <jonas.s.martins@hotmail.com>
2016-11-19 23:22:07 -0300

    Creating Recipe and Chef models

---
+++ app/models/recipe.rb
@@ -0,0 +1,27 @@
+class Recipe < ActiveRecord::Base
+=begin
+	/**
+	 *	 rails generate model Recipe name:string summary:text description:text
+	 *
+	 * Isso vai gerar tanto a tabela quanto o model 
+	 * para poder acessar os atributos da tabela
+	 * sempre com essa regra, nome do Objeto, model
+	 * em letra letra maiúscula primeiro e singular
+	 * isso gera esssa classe e o arquivo migrate
+	 * relacionado aquele objeto aquela tabela
+	 *
+	 * http://guides.rubyonrails.org/getting_started.html
+	 *
+	 * ctrl+f "generate model" para maiores detalhes 
+	 *
+	 * Notar que a tabela é criada no banco de dados, no 
+	 * postgres recipes nessa notação no plural refernte
+	 * a esse objeto.
+	 *
+	 * ápós isso basta usar o comando para gerar as migrações
+	 * com: rake db:migrate isso cria os arquivos e 
+	 * a tabela, executando o arquivo referente migration.rb
+	 * referente ao comando acionado anteriormente
+	 */
+=end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140707111715) do
+ActiveRecord::Schema.define(version: 20161120022042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chefs", force: :cascade do |t|
+    t.string   "chefname"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string   "name"
+    t.text     "summary"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "widgets", force: :cascade do |t|
     t.string   "name"
