571c56ba817f75399299182a26a4e265de0146b8
JonasMartins <jonas.s.martins@hotmail.com>
2016-11-20 01:18:28 -0300

    Add validations to Chef model

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,2 +1,17 @@
 class Chef < ActiveRecord::Base
+
+	# antes de salvar, já adiciona a função
+	# downcase na entrada
+	
+	before_save { self.email = email.downcase }
+	
+	validates :chefname, presence: true, length:
+	 {minumum: 3, maximum: 40}
+	#found at: http://emailregex.com/
+	VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+	validates :email, presence: true, length: 
+	{maximum: 105},uniqueness: {case_sensitive: false}, 
+	format: {with: VALID_EMAIL_REGEX}
+
 end
+ 
