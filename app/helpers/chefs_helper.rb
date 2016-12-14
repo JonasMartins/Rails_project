cba073515d6383e1079c197e526134e760e2b626
JonasMartins <jonas.s.martins@hotmail.com>
2016-12-14 17:13:11 -0300

    Preparing heroku commit

--- app/helpers/chefs_helper.rb
+++ app/helpers/chefs_helper.rb
@@ -2,7 +2,7 @@ module ChefsHelper
 
 	# Colocar todos os países aqui e pegar pela view
 	@countries = Hash['Afghanistan' => '48/Afghanistan.png', 'Albania' => '48/Albania.png',
-	'Algeria' => '48/Algeria.png', 'Andorra' => '48/Andorra.png', 'United Kingdom' => '48/United Kingdom(Great Britain).png']
+	'Algeria' => '48/Algeria.png', 'Andorra' => '48/Andorra.png', 'Scotland' => '48/Scotland.png','United Kingdom' => '48/United Kingdom(Great Britain).png']
 	def self.get_countries
 		return @countries
 	end
